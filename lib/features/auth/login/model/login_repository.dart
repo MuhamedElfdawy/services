88df09f055bb93ea6deb0e362a3d73e74dc357e1
aymanomara <aymanomara55@gmail.com>
2024-04-19 23:03:50 +0200

    fix android local host issue

--- lib/features/auth/login/model/login_repository.dart
+++ lib/features/auth/login/model/login_repository.dart
@@ -11,7 +11,7 @@ class LoginModel {
         validateStatus: (status) => true,
       ),
     );
-    final response = await dio.post('https://localhost:7033/api/Auth/Login', data: {
+    final response = await dio.post('https://10.0.2.2:7033/api/Auth/Login', data: {
       'userName': email,
       'password': password,
     });
